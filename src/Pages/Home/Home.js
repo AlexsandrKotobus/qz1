dcfac6a7d5463a32b189055571eca6e1f945978e
Julia <alexsandr.kotobus@gmail.com>
2023-12-05 12:00:17 +0300

    +Categories

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,36 @@
 ﻿import React from 'react';
+import { MenuItem, TextField } from '@mui/material';
+//точки - на 2 папки выше, 
+import Categories from '../../Data/Categories';
 import './Home.css';
+
+
 
 const Home = () => {
     return (
         <div className='content'>
             <div  className='setting'>
                 <span style={{fontSize: 30}} >Quiz Settings</span>
-                <div className='settings_select'>
-                    {/* <TextField></TextField> */}
+                <div className='settings__select'>
+                    <TextField style={{marginBottom: 30}}  
+                    label="Enter Your Name" 
+                    variant="outlined" />
+                    <TextField  style={{marginBottom: 30}}
+                    select
+                    label="Select Category"
+                    variant="outlined"
+                    >
+                        
+                        {
+                        Categories.map((cat) => (
+                            <MenuItem key = {cat.category} value={cat.value}>
+                                {cat.category}
+
+                            </MenuItem>
+                            
+                        ))}
+                        
+                    </TextField>
 
 
                 </div>
